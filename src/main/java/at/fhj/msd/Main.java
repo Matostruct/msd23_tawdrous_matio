df2f29cddfca5cde16cd42facc212abed64199a3
Matio Tawdrous <matio.tawdrous@fh-joannneum.at>
2024-05-16 10:37:30 +0200

    initialize log4j

--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,5 +1,9 @@
 package at.fhj.msd;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * msd23_tawdrous_matio
  * This is the main class that will run the calculator
@@ -9,12 +13,14 @@ package at.fhj.msd;
 public class Main {
     public static void main(String[] args) {
         Calculator c = new Calculator();
+        Logger logger = LogManager.getLogger();
+        logger.info("Starting the calculator");
         System.out.println("Add: " + c.add(1,1));
         System.out.println("Minus: " + c.minus(2,2));
         System.out.println("Multiply: " + c.multiply(3,3));
         System.out.println("Divide: " + c.divide(4,4));
         System.out.println("Factorial: " + c.factorial(5));
-
+        logger.error("Stopping the calculator");
         System.out.println("-------------------------------------");
         System.out.println("Matio Tawdrous");
         System.out.println("-------------------------------------");
